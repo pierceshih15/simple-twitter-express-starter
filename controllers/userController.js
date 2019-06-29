ef10932ffb5ba7ee14f817ea6bfb7e92e37a8376
wanaka <asd8116@gmail.com>
2019-06-29 09:30:07 +0800

    Confirm user password and unique email & name

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt-nodejs')
 const db = require('../models')
 const User = db.User
+const Sequelize = require('sequelize')
 
 const userController = {
   signUpPage: (req, res) => {
@@ -8,13 +9,37 @@ const userController = {
   },
 
   signUp: (req, res) => {
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    }).then(user => {
-      return res.redirect('/signin')
-    })
+    if (req.body.passwordCheck !== req.body.password) {
+      req.flash('error_messages', '兩次密碼輸入不同！')
+      return res.redirect('/signup')
+    } else {
+      User.findOne({
+        where: {
+          [Sequelize.Op.or]: [
+            { email: req.body.email },
+            { name: req.body.name }
+          ]
+        }
+      }).then(user => {
+        if (user) {
+          req.flash('error_messages', '名稱或是信箱重複！')
+          return res.redirect('/signup')
+        } else {
+          User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(
+              req.body.password,
+              bcrypt.genSaltSync(10),
+              null
+            )
+          }).then(user => {
+            req.flash('success_messages', '成功註冊帳號！')
+            return res.redirect('/signin')
+          })
+        }
+      })
+    }
   }
 }
 
