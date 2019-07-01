3626544e56fe7edf5b95b838ff60f6985e81165a
Lianginger <jackyliang444@hotmail.com.tw>
2019-07-01 17:19:51 +0800

    Add topTenUsers on tweets page

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -9,12 +9,27 @@ const tweetController = {
     const tweets = await Tweet.findAll({
       include: [User, Like, Reply]
     })
+    const users = await User.findAll({
+      include: [{ model: User, as: 'Followers' }]
+    })
+
     let allTweets = tweets.map(tweet => ({
       ...tweet.dataValues,
       numOfReply: tweet.Replies.length,
       numOfLike: tweet.Likes.length
     }))
-    res.render('tweets', { allTweets })
+
+    let topTenUsers = users.map(user => ({
+      ...user.dataValues,
+      FollowerCount: user.Followers.length,
+      // 判斷目前登入使用者是否已追蹤該 User 物件
+      isFollowed: req.user.Followings.map(following => following.id).includes(user.id)
+    }))
+
+    topTenUsers = topTenUsers.sort((a, b) => {
+      return b.FollowerCount - a.FollowerCount
+    })
+    res.render('tweets', { allTweets, topTenUsers })
   }
 }
 
