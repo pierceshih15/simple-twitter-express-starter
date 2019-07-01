02d32e0d6eb424b80f4bd1d2e9223d43f0bf3053
pierce <pierceshih@gmail.com>
2019-07-01 22:59:30 +0800

    Update adminPage and create pagination feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,7 +3,40 @@ const Tweet = db.Tweet
 
 const adminController = {
   getTweets: (req, res) => {
-    return res.render('admin/tweets')
+    let offset = 0
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
+
+    Tweet.findAndCountAll({
+      include: User,
+      offset: offset,
+      limit: pageLimit
+    }).then(result => {
+      console.log(result.count, result)
+
+      let page = Number(req.query.page) || 1
+      let pages = Math.ceil(result.count / pageLimit)
+      let totalPage = Array.from({
+        length: pages
+      }).map((item, index) => index + 1)
+      let prev = page - 1 ? 1 : page - 1
+      let next = page + 1 ? pages : pages + 1
+
+      const data = result.rows.map(r => ({
+        ...r.dataValues,
+        description: r.dataValues.description.substring(0, 50)
+      }))
+      return res.render('admin/tweets', {
+        tweets: data,
+        page: page,
+        pageLimit: pageLimit,
+        totalPage: totalPage,
+        prev: prev,
+        next: next
+      })
+    })
+  },
   }
 }
 
