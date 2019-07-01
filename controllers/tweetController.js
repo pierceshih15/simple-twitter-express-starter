48a396f5fa4d248ccc57c4b6c516495d85f17bee
Lianginger <jackyliang444@hotmail.com.tw>
2019-07-01 15:31:03 +0800

    Update tweets page to display tweet form and all tweets

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -1,9 +1,20 @@
 const db = require('../models')
 const Tweet = db.Tweet
+const User = db.User
+const Like = db.Like
+const Reply = db.Reply
 
 const tweetController = {
-  getTweets: (req, res) => {
-    return res.render('tweets')
+  getTweets: async (req, res) => {
+    const tweets = await Tweet.findAll({
+      include: [User, Like, Reply]
+    })
+    let allTweets = tweets.map(tweet => ({
+      ...tweet.dataValues,
+      numOfReply: tweet.Replies.length,
+      numOfLike: tweet.Likes.length
+    }))
+    res.render('tweets', { allTweets })
   }
 }
 
