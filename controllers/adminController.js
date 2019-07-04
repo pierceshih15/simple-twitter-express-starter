e21b8ad5188ca2ded8a4616a696c38f3060abec3
pierce <pierceshih@gmail.com>
2019-07-04 16:33:47 +0800

    Updated admin page and remove pagination.

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,41 +5,40 @@ const User = db.User
 const Reply = db.Reply
 const Like = db.Like
 
-const pageLimit = 5
+// const pageLimit = 5
 
 const adminController = {
   getTweets: (req, res) => {
-    let offset = 0
-    if (req.query.page) {
-      offset = (req.query.page - 1) * pageLimit
-    }
+    // let offset = 0
+    // if (req.query.page) {
+    //   offset = (req.query.page - 1) * pageLimit
+    // }
 
     Tweet.findAndCountAll({
-      include: User,
-      offset: offset,
-      limit: pageLimit
+      include: [User, Like, Reply]
+      // offset: offset,
+      // limit: pageLimit
     }).then(result => {
-      // console.log(result.count, result)
-
-      let page = Number(req.query.page) || 1
-      let pages = Math.ceil(result.count / pageLimit)
-      let totalPage = Array.from({
-        length: pages
-      }).map((item, index) => index + 1)
-      let prev = page - 1 ? 1 : page - 1
-      let next = page + 1 ? pages : pages + 1
+      // let page = Number(req.query.page) || 1
+      // let pages = Math.ceil(result.length / pageLimit)
+      // let totalPage = Array.from({
+      //   length: pages
+      // }).map((item, index) => index + 1)
+      // let prev = page - 1 ? 1 : page - 1
+      // let next = page + 1 ? pages : pages + 1
 
       const data = result.rows.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50)
       }))
+
       return res.render('admin/tweets', {
-        tweets: data,
-        page: page,
-        pageLimit: pageLimit,
-        totalPage: totalPage,
-        prev: prev,
-        next: next
+        tweets: data
+        // page: page,
+        // pageLimit: pageLimit,
+        // totalPage: totalPage,
+        // prev: prev,
+        // next: next
       })
     })
   },
